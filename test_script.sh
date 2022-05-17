f09fb727794c009126e12e186a248654d5b6e89a
Maxat Akbanov <30049303+Brain2life@users.noreply.github.com>
2022-05-17 17:16:35 +0600

    Add test_script.sh

---
+++ test_script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "This is a sample shell script from GitHub project which sums two numbers"
+
+SUM = $( ($FIRST_NUMBER + $SECOND_NUMBER) )
+
+echo "$SUM"
+echo "Script was executed"
