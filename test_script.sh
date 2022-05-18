a3bf49cd05a075056135289a3bf423d8cc45f0e0
Maxat Akbanov <30049303+Brain2life@users.noreply.github.com>
2022-05-18 13:27:28 +0600

    Refactor: Delete spaces

--- test_script.sh
+++ test_script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo "This is a sample shell script from GitHub project which sums two numbers"
+echo "This is a sample shell script from GitHub project which returns the sum of two numbers"
 
-SUM = $( ($FIRST_NUMBER + $SECOND_NUMBER) )
+SUM=$(($FIRST_NUMBER+$SECOND_NUMBER))
 
 echo "$SUM"
 echo "Script was executed"
