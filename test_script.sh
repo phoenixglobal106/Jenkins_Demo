5635f4f47c0e9edadede1b602fbe0e4587042acb
phoenixglobal106 <105905176+phoenixglobal106@users.noreply.github.com>
2022-05-21 15:53:28 +0530

    Update test_script.sh

--- test_script.sh
+++ test_script.sh
@@ -2,7 +2,7 @@
 
 echo "This is a sample shell script from GitHub project which returns the diff of two numbers"
 
-Diff=$(($FIRST_NUMBER-$SECOND_NUMBER))
+Diff=$(($FIRST_NUMBER+$SECOND_NUMBER))
 
 echo "$SUM"
 echo "Script was executed"
